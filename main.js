6b476d1d4f9a7cfe13e6b98a542f575be6465fcb
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-25 13:07:06 -0500

    Updated the usage of withRuntimVar and the main method to use the variables.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 const { Compiler } = require('@adobe/htlengine');
 const requireFromString = require('require-from-string');
 
-let headerComponent = {
-  title: 'Hello, World!',
-  description: 'Some stuff.'
+const GLOBALS = {
+  headerComponent: {
+    title: 'Hello, World!',
+    description: 'Some stuff.'
+  }
 };
 
-// Why does "withRuntimeVar" take a string of a variable name?!
 const compiler = new Compiler()
   .withOutputDirectory('')
   .includeRuntime(true)
-  .withRuntimeVar('headerComponent')
+  .withRuntimeVar(Object.keys(GLOBALS))
   .withRuntimeGlobalName('it');
 
 async function main() {
@@ -18,7 +19,7 @@ async function main() {
   const js = await compiler.compileToString("<h1>${headerComponent.title}</h1><p>${headerComponent.description}</p>");
 
   const { main } = requireFromString(js);
-  const { body } = await main({});
+  const { body } = await main(GLOBALS);
   return body;
 }
 
