8fd1131ccb88dd45fed97d791fc037a18cf4694f
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-25 13:44:57 -0500

    Added example of data-sly-use

--- main.js
+++ main.js
@@ -2,24 +2,15 @@ const { Compiler } = require('@adobe/htlengine');
 const requireFromString = require('require-from-string');
 const path = require('path');
 const fs = require('fs-extra');
-
-const GLOBALS = {
-  headerComponent: {
-    title: 'Hello, World!',
-    description: 'Some stuff.'
-  }
-};
-
 const compiler = new Compiler()
   .includeRuntime(true)
-  .withRuntimeVar(Object.keys(GLOBALS))
   .withRuntimeGlobalName('it');
 
 async function main() {
   const template = await fs.readFile(path.resolve(__dirname, 'template.html'), 'utf-8');
   const js = await compiler.compileToString(template);
   const { main } = requireFromString(js);
-  const { body } = await main(GLOBALS);
+  const { body } = await main({});
   return body;
 }
 
