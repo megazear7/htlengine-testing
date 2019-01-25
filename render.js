6473c1db080c1c985d669eef9236f87e60c1c724
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-25 15:10:29 -0500

    Exporting the method that renders the htl template.

--- render.js
+++ render.js
@@ -1,18 +1,14 @@
 const { Compiler } = require('@adobe/htlengine');
 const requireFromString = require('require-from-string');
-const path = require('path');
-const fs = require('fs-extra');
 const compiler = new Compiler()
   .includeRuntime(true)
   .withRuntimeGlobalName('it');
 
-async function main(file) {
-  const template = await fs.readFile(path.resolve(__dirname, file), 'utf-8');
+async function render(template) {
   const js = await compiler.compileToString(template);
   const { main } = requireFromString(js);
   const { body } = await main({});
   return body;
 }
 
-main('examplecomponent.html')
-.then(body => console.log(body));
+module.exports = render;
