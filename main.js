96878d6903324f8c667d8e59eb91eb507bdc4e8d
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-01-25 12:36:04 -0500

    Basic example setup - The HTL JavaScript Engine has a very strange API... I hope I am doing something wrong.

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log('Hello, World');
+const { Compiler } = require('@adobe/htlengine');
+const { Runtime } = require('@adobe/htlengine');
+
+let headerComponent = {
+  title: 'Hello, World!',
+  description: 'Some stuff.'
+};
+
+// Why in the world does "withRuntimeVar" take a string of a variable name????
+const compiler = new Compiler()
+      .withOutputDirectory('')
+      .includeRuntime(true)
+      .withRuntimeVar('headerComponent')
+      .withRuntimeGlobalName('it');
+
+compiler.compileToString(`
+  <h1>${headerComponent.title}</h1>
+  <p>${headerComponent.description}</p>
+`).then(js => {
+  // Am I using the right method to compile? Why do I have to eval a string of JavaScript?
+  eval(js);
+
+  main = async function main(resource) {
+    const runtime = new Runtime();
+    runtime.setGlobal(resource);
+    await run(runtime);
+    return {
+      body: runtime.stream
+    };
+  };
+
+  // What is supposed to get passed into this method?
+  result = main('');
+
+  result.then(({body}) => console.log(body));
+});
